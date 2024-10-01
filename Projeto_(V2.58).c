4f5dd7b8272dbcdd4a1d1cad90f099e53f6f5836
IvysonWNM <ivysonnunes@hotmail.com>
2024-09-30 21:57:41 -0300

    Update Projeto_(V2.58).c
    Adiciona uma função para navegar entre os módulos do sistema; no entanto, ainda está incompleta.

--- Projeto_(V2.58).c
+++ Projeto_(V2.58).c
@@ -66,6 +66,7 @@ int main(void) {
 // Funções
 
 void tela_principal(void) {
+    char op;
     system("clear||cls");
     printf("\n");
     printf("┌─────────────────────────────────────────────────────────────────────────────┐\n");
@@ -87,6 +88,13 @@ void tela_principal(void) {
     printf("| 5. Módulo Relatórios                                                        |\n");
     printf("| 0. Sair                                                                     |\n");
     printf("└─────────────────────────────────────────────────────────────────────────────┘\n");
+    printf("──────────────────────────────────────────────────────────────────────────────\n");
+    printf("\n");
+    printf("            Escolha a opção desejada: ");
+    scanf("%c", &op);
+    getchar();
+    printf("\n");
+    printf("──────────────────────────────────────────────────────────────────────────────\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
     printf("\n");
@@ -184,7 +192,8 @@ void exclui_cliente(void) {
     printf("\n");
 }
 
-void tela_veiculo(void) {   
+void tela_veiculo(void) {
+    char op;   
     system("clear||cls");
     printf("\n");
     printf("┌─────────────────────────────────────────────────────────────────────────────┐\n");
@@ -200,10 +209,17 @@ void tela_veiculo(void) {
     printf("| 0. Sair                                                                     |\n");
     printf("|                                                                             |\n");
     printf("└─────────────────────────────────────────────────────────────────────────────┘\n");
-    printf("\n"); 
-    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n"); 
-    getchar(); 
-} 
+    printf("──────────────────────────────────────────────────────────────────────────────\n");
+    printf("\n");
+    printf("            Escolha a opção desejada: ");
+    scanf("%c", &op);
+    getchar();
+    printf("\n");
+    printf("──────────────────────────────────────────────────────────────────────────────\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+    printf("\n");
+}
 
 void cadastra_veiculo(void) {
     
@@ -294,6 +310,7 @@ void exclui_veiculo(void) {
 } 
 
 void tela_funcionario(void) { 
+    char op;
     system("clear||cls");
     printf("\n");
     printf("┌─────────────────────────────────────────────────────────────────────────────┐\n");
@@ -309,10 +326,17 @@ void tela_funcionario(void) {
     printf("| 0. Sair                                                                     |\n");
     printf("|                                                                             |\n");
     printf("└─────────────────────────────────────────────────────────────────────────────┘\n"); 
+    printf("──────────────────────────────────────────────────────────────────────────────\n");
     printf("\n");
-    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n"); 
+    printf("            Escolha a opção desejada: ");
+    scanf("%c", &op);
     getchar();
-} 
+    printf("\n");
+    printf("──────────────────────────────────────────────────────────────────────────────\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+    printf("\n");
+}
 
 void cadastra_funcionario(void) {
     system("clear||cls");
@@ -416,6 +440,7 @@ void tela_sobre(void) {
     printf("└─────────────────────────────────────────────────────────────────────────────┘\n");
     printf("┌─────────────────────────────────────────────────────────────────────────────┐\n");
     printf("|                    Sistema de Gestão Para Concessionária                    |\n");
+    printf("|                                                                             |\n");
     printf("| Programa universitário da disciplina de PROGRAMAÇÃO (DCT1106) da UFRN.      |\n");
     printf("| Escrito na linguagem de programação C, fornecendo uma plataforma de gestão  |\n");
     printf("| para uma concessionária de veículos, com interfaces intuitivas divididas em |\n");
@@ -456,5 +481,6 @@ void tela_equipe(void) {
     getchar();
     printf("\n");
 }
+
 
 
