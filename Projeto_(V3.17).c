15ef8be9dae2f590aa1f200eb65408372052a03a
IvysonWNM <ivysonnunes@hotmail.com>
2024-10-12 22:35:52 -0300

    Update Projeto_(V3.17).c
    Troquei número de páginas (num_pag) para opção (op) para deixar mais legível organizado

--- Projeto_(V3.17).c
+++ Projeto_(V3.17).c
@@ -11,74 +11,78 @@
 // Projeto V2.98
 
 // ################################
-// Assinatura das Funções
-void tela_principal(void);
+// Assinatura das Funções ( telas principais)
+int tela_principal(void);
 void tela_cliente(void);
+void tela_veiculo(void);
+void tela_funcionario(void);
+void tela_venda(void);
+void tela_sobre(void);
+void tela_equipe(void);
+
+
+// Assinatura das Funções (telas secundárias)
+
 void cadastra_cliente(void);
 void pesquisa_cliente(void);
 void atualiza_cliente(void);
 void exclui_cliente(void);
 
-void tela_veiculo(void);
+
 void cadastra_veiculo(void);
 void pesquisa_veiculo(void);
 void atualiza_veiculo(void);
 void exclui_veiculo(void);
 
-void tela_funcionario(void);
+
 void cadastra_funcionario(void);
 void pesquisa_funcionario(void);
 void atualiza_funcionario(void);
 void exclui_funcionario(void);
 
-void tela_venda(void);
+
 void cadastra_venda(void);
 void pesquisa_venda(void);
 void atualiza_venda(void);
 void exclui_venda(void);
 
-void tela_sobre(void);
-void tela_equipe(void);
 
 // ################################
 // Programa Principal
 int main(void) {
-    tela_principal();
-    tela_cliente();
-    cadastra_cliente();
-    pesquisa_cliente();
-    atualiza_cliente();
-    exclui_cliente();
-
-    tela_veiculo();
-    cadastra_veiculo();
-    pesquisa_veiculo();
-    atualiza_veiculo();
-    exclui_veiculo();
-    
-    tela_funcionario();
-    cadastra_funcionario();
-    pesquisa_funcionario();
-    atualiza_funcionario();
-    exclui_funcionario();
-   
-    tela_venda();
-    cadastra_venda();
-    pesquisa_venda();
-    atualiza_venda();
-    exclui_venda();
-
-    tela_sobre();
-    tela_equipe();
+    int op;
+    do {
+        op = tela_principal();
+        switch (op) {
+           case 1: 
+               tela_cliente();
+               break;
+           case 2: 
+               tela_veiculo();
+		break;
+           case 3: 
+               tela_funcionario();
+               break;
+           case 4: 
+               tela_venda();
+               break;
+           case 5: 
+               tela_sobre();
+               break;
+           case 6: 
+               tela_equipe();
+               break;
+        }
+    } while (op != 0);
+    printf("The End\n"); 
     return 0;
 }
-
 
 // ################################
 // Funções
 
-void tela_principal(void) {
-    char num_pag;
+int tela_principal(void) {
+    int op;
     system("clear||cls");
     printf("\n");
     printf("┌─────────────────────────────────────────────────────────────────────────────┐\n");
@@ -97,23 +101,23 @@ void tela_principal(void) {
     printf("| 2. Módulo Veículos                                                          |\n");
     printf("| 3. Módulo Funcionário                                                       |\n");
     printf("| 4. Módulo Venda                                                             |\n");
-    printf("| 5. Módulo Relatórios                                                        |\n");
+    printf("| 5. Módulo Sobre                                                             |\n");
+    printf("| 6. Módulo Equipe                                                            |\n");
     printf("| 0. Sair                                                                     |\n");
     printf("|                                                                             |\n");
     printf("└─────────────────────────────────────────────────────────────────────────────┘\n");
     printf("──────────────────────────────────────────────────────────────────────────────\n");
     printf("\n");
     printf("            Escolha a opção desejada: ");
-    scanf("%c", &num_pag);
+    scanf("%d", &op);
     getchar();
     printf("\n");
     printf("──────────────────────────────────────────────────────────────────────────────\n");
-    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-    getchar();
-    printf("\n");
+    return op;
 }
 
 void tela_cliente(void) {
+    char op;
     system("clear||cls");
     printf("\n");
     printf("┌─────────────────────────────────────────────────────────────────────────────┐\n");
@@ -129,9 +133,20 @@ void tela_cliente(void) {
     printf("| 0. Voltar ao Menu                                                           |\n");
     printf("|                                                                             |\n");
     printf("└─────────────────────────────────────────────────────────────────────────────┘\n");
+    printf("──────────────────────────────────────────────────────────────────────────────\n");
+    printf("\n");
+    printf("            Escolha a opção desejada: ");
+    scanf("%c", &op);
+    getchar();
+    printf("\n");
+    printf("──────────────────────────────────────────────────────────────────────────────\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
     printf("\n");
+    cadastra_cliente();
+    pesquisa_cliente();
+    atualiza_cliente();
+    exclui_cliente();
 }
 
 void cadastra_cliente(void) {	
@@ -212,7 +227,7 @@ void exclui_cliente(void) {
 }
 
 void tela_veiculo(void) {
-    char num_pag;   
+    char op;   
     system("clear||cls");
     printf("\n");
     printf("┌─────────────────────────────────────────────────────────────────────────────┐\n");
@@ -231,13 +246,17 @@ void tela_veiculo(void) {
     printf("──────────────────────────────────────────────────────────────────────────────\n");
     printf("\n");
     printf("            Escolha a opção desejada: ");
-    scanf("%c", &num_pag);
+    scanf("%c", &op);
     getchar();
     printf("\n");
     printf("──────────────────────────────────────────────────────────────────────────────\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
     printf("\n");
+    cadastra_veiculo();
+    pesquisa_veiculo();
+    atualiza_veiculo();
+    exclui_veiculo();
 }
 
 void cadastra_veiculo(void) {
@@ -316,7 +335,7 @@ void exclui_veiculo(void) {
 } 
 
 void tela_funcionario(void) { 
-    char num_pag;
+    char op;
     system("clear||cls");
     printf("\n");
     printf("┌─────────────────────────────────────────────────────────────────────────────┐\n");
@@ -335,13 +354,17 @@ void tela_funcionario(void) {
     printf("──────────────────────────────────────────────────────────────────────────────\n");
     printf("\n");
     printf("            Escolha a opção desejada: ");
-    scanf("%c", &num_pag);
+    scanf("%c", &op);
     getchar();
     printf("\n");
     printf("──────────────────────────────────────────────────────────────────────────────\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
     printf("\n");
+    cadastra_funcionario();
+    pesquisa_funcionario();
+    atualiza_funcionario();
+    exclui_funcionario();
 }
 
 void cadastra_funcionario(void) {
@@ -422,7 +445,7 @@ void exclui_funcionario(void) {
 }   
 
 void tela_venda(void) { 
-    char num_pag;
+    char op;
     system("clear||cls");
     printf("\n");
     printf("┌─────────────────────────────────────────────────────────────────────────────┐\n");
@@ -441,13 +464,17 @@ void tela_venda(void) {
     printf("──────────────────────────────────────────────────────────────────────────────\n");
     printf("\n");
     printf("            Escolha a opção desejada: ");
-    scanf("%c", &num_pag);
+    scanf("%c", &op);
     getchar();
     printf("\n");
     printf("──────────────────────────────────────────────────────────────────────────────\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
     printf("\n");
+    cadastra_venda();
+    pesquisa_venda();
+    atualiza_venda();
+    exclui_venda();
 }
 
 void cadastra_venda(void) {
@@ -582,7 +609,3 @@ void tela_equipe(void) {
     getchar();
     printf("\n");
 }
-
-
-
-
