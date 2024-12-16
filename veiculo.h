5294aecc40c4e014848c01674b1a89e19180ade5
xonazz <robinsojjcs@gmail.com>
2024-12-15 22:54:18 -0300

    cabeçalho
    pequena mudança no cabeçalho da função

--- veiculo.h
+++ veiculo.h
@@ -1,5 +1,10 @@
+#ifndef VEICULO_H
+#define VEICULO_H
+
 void tela_veiculo(void);
 void cadastra_veiculo(void);
 void pesquisa_veiculo(void);
 void atualiza_veiculo(void);
-void exclui_veiculo(void);
+void exclui_veiculo(void); 
+
+#endif
