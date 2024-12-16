d56132b87b83a2172b7ce470e56c53ba4e2d808b
xonazz <robinsojjcs@gmail.com>
2024-12-15 22:37:52 -0300

    mudança no cabeçalho
    pequena mudança no cabeçalho.

--- cliente.h
+++ cliente.h
@@ -1,5 +1,9 @@
+#ifndef CLIENTE_H
+#define CLIENTE_H
 void tela_cliente(void);
 void cadastra_cliente(void);
 void pesquisa_cliente(void);
 void atualiza_cliente(void);
-void exclui_cliente(void);
+void exclui_cliente(void); 
+
+#endif
