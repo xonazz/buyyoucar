010e7c05a3d4f94f0ef9e8b5746c117d0bcc55af
xonazz <robinsojjcs@gmail.com>
2024-12-15 22:46:22 -0300

    mudança no cabeçalho
    pequena mudança no cabeçalho da função

--- funcionario.h
+++ funcionario.h
@@ -1,5 +1,10 @@
+#ifndef FUNCIONARIO_H
+#define FUNCIONARIO_H
+
 void tela_funcionario(void); 
 void cadastra_funcionario(void); 
 void pesquisa_funcionario(void); 
 void atualiza_funcionario(void); 
 void exclui_funcionario(void); 
+
+#endif
