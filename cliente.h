7015109c9d407aff4b6e0154ece004f2a072cff0
xonazz <robinsojjcs@gmail.com>
2025-01-21 15:04:35 -0300

    alteração cliente.h
    Alteração nas funções e struct.

--- cliente.h
+++ cliente.h
@@ -1,9 +1,29 @@
-#ifndef CLIENTE_H
-#define CLIENTE_H
+#ifndef CLIENTE_H 
+#define CLIENTE_H 
+
+#include <stdio.h>
+
+// Struct p/ dados do cliente 
+typedef struct {
+    char status;          // Status do cliente (ativo, inativo, etc)
+    char nome[52];        // Nome como array estático
+    char cpf[13];         // CPF com tamanho fixo
+    char email[258];      // E-mail como array estático
+    char cnh[12];        // n° da carteira de motorista estático
+    char fone[15];        // Telefone como array estático
+} Cliente;
+
+
+
 void tela_cliente(void);
 void cadastra_cliente(void);
 void pesquisa_cliente(void);
 void atualiza_cliente(void);
-void exclui_cliente(void); 
+void exclui_cliente(void);
 
-#endif
+void grava_cliente(FILE *fp, Cliente *cliente);
+void busca_cliente(const char *cpf_busca);
+
+int verificaCpfCadastrado(const char *cpf);
+
+#endif 
