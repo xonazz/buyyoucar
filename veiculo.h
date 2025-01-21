09e4eafed869ed506099162593ce0b8c158c5b2a
xonazz <robinsojjcs@gmail.com>
2025-01-21 15:14:44 -0300

    Update veiculo.h
    Alteração nas funções e struct.

--- veiculo.h
+++ veiculo.h
@@ -1,10 +1,24 @@
 #ifndef VEICULO_H
 #define VEICULO_H
 
+typedef struct{
+
+    char sit;
+    char placa[8];
+    char marca[52];
+    char modelo[52];
+    char anodf[5];
+
+}Veiculo;
+
 void tela_veiculo(void);
 void cadastra_veiculo(void);
 void pesquisa_veiculo(void);
 void atualiza_veiculo(void);
-void exclui_veiculo(void); 
+void exclui_veiculo(void);
+
+void lePlacaBusca(char *placa_busca);
+void busca_placa(const char *placa_busca); 
+int verificaPlacaCadastrada(const char *placa);
 
 #endif
