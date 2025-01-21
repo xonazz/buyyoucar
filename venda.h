409a9694302686ceade4be867e28223b0d4adc4f
xonazz <robinsojjcs@gmail.com>
2025-01-21 15:16:26 -0300

    Update venda.h
    Alteração nas funções e struct

--- venda.h
+++ venda.h
@@ -1,5 +1,30 @@
+#ifndef VENDA_H
+#define VENDA_H 
+
+
+typedef struct{
+
+char cpfc[13];
+char cpff[13];
+char placa[8];
+char data[15];
+float valor;
+char parce[3]; 
+char sit;
+
+}Venda;
+
+
 void tela_venda(void);
 void cadastra_venda(void);
 void pesquisa_venda(void); 
 void atualiza_venda(void); 
-void exclui_venda(void); 
+void exclui_venda(void);  
+
+int verificaPlacaCadastrada(const char *placa); 
+void leCpfVBusca (char *cpf_busca); 
+char buscarProfessor(char*);
+
+
+
+#endif
